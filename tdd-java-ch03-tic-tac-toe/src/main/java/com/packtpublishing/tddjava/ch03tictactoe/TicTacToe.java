416080c230b0e212e848608a7b90a3007953e4c1
sfw <u18a@sfw.com>
2018-05-01 12:38:38 +0800

    --refactoring isWin()--

--- tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ tdd-java-ch03-tic-tac-toe/src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -28,10 +28,16 @@ public class TicTacToe {
         return "No winner";
     }
 
-
     private boolean isWin(){
         int playerTotal = lastPlayer * SIZE;
+        char diagonal1 = '\0';
+        char diagonal2 = '\0';
+
         for(int i = 0; i < SIZE; i++){
+
+            diagonal1 += board[i][i];
+            diagonal2 += board[i][SIZE - i - 1];
+
             if(board[0][i] + board[1][i] + board[2][i]
                     == playerTotal){
                 return true;
@@ -44,15 +50,37 @@ public class TicTacToe {
             }
         }
 
-        if(board[0][0] + board[1][1] + board[2][2]
-                == playerTotal){
-            return true;
-        }else if(board[0][2] + board[1][1] + board[2][0]
-                == playerTotal) {
+        if(diagonal1 == playerTotal || diagonal2 == playerTotal){
             return true;
         }
+
         return false;
     }
+
+//    private boolean isWin(){
+//        int playerTotal = lastPlayer * SIZE;
+//        for(int i = 0; i < SIZE; i++){
+//            if(board[0][i] + board[1][i] + board[2][i]
+//                    == playerTotal){
+//                return true;
+//            }else if(board[i][0] + board[i][1] + board[i][2]
+//                    == playerTotal){
+//                return true;
+//            }
+//            else if(playerTotal == '\0') {
+//                return true;
+//            }
+//        }
+//
+//        if(board[0][0] + board[1][1] + board[2][2]
+//                == playerTotal){
+//            return true;
+//        }else if(board[0][2] + board[1][1] + board[2][0]
+//                == playerTotal) {
+//            return true;
+//        }
+//        return false;
+//    }
 
     public char nextPlayer(){
         if(lastPlayer == 'X'){
